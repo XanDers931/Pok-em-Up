b83a57667168b1e3ece7a4a416542cefdc5ef040
Axel Saint-maxin <axel.saintmaxin.etu@unvi-lille.fr>
2024-03-14 16:50:48 +0100

    finish merge

--- client/src/main.js
+++ client/src/main.js
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-/**
- * Canvas
- */
-const canvas = document.querySelector('.gameCanvas'),
-	context = canvas.getContext('2d');
-
-/**
- * Canvas implementation
- */
-
-// me permet d'attendre que le canvas soit charger mais mal fait
-// setTimeout génére une erreur dans le code
-setTimeout(() => {
-	getInfo(canvas);
-}, 100);
-
-import { getCanvas } from './vue/draw.js';
-import { getInfo } from './vue/canvasInfo.js';
-
-getCanvas(canvas);
-
-=======
->>>>>>> bfed230259f20bc133867ec44b3571d892a56947
 import View from './View.js';
 import GameView from './GameView.js';
 import Router from './Router.js';
@@ -38,50 +14,5 @@ import { io } from 'socket.io-client';
 const socket = io();
 
 Router.routes = routes;
-<<<<<<< HEAD
-Router.navigate('/game');
-Router.setMenuElement(document.querySelector('#menu')); // TODO ajouter dans le html la classe mainMenu a la liste des boutons du menu
-
-/**
- *  Code
- */
-
-import { Background } from './vue/inGame/background.js';
-let bg = new Background(canvas.height, canvas.clientHeight);
-
-import { Player } from './modele/inGame/player.js';
-let p = new Player(canvas, 0, 0);
-
-/**
- *  Render
- */
-
-requestAnimationFrame(render);
-
-function render() {
-	context.clearRect(0, 0, canvas.width, canvas.height);
-
-	if (bg.getReady()) {
-		bg.display();
-	}
-	if (p.getReady()) {
-		p.display();
-	}
-	context.stroke();
-	requestAnimationFrame(render);
-}
-
-/**
- * Canvas resize for each
- */
-const canvasResizeObserver = new ResizeObserver(() => resampleCanvas());
-canvasResizeObserver.observe(canvas);
-
-function resampleCanvas() {
-	canvas.width = canvas.clientWidth;
-	canvas.height = canvas.clientHeight;
-}
-=======
 Router.navigate('/');
 Router.setMenuElement(document.querySelector('.menuButtons')); // TODO ajouter dans le html la classe mainMenu a la liste des boutons du menu
->>>>>>> bfed230259f20bc133867ec44b3571d892a56947
