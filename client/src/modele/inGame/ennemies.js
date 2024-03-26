99650c7b1ad2787f9b507676f7470c6d182ccf48
alexandre.martel.etu <alexandre.martel.etu@univ-lille.fr>
2024-03-26 15:30:28 +0100

    Ennemies Init

---
+++ client/src/modele/inGame/ennemies.js
@@ -0,0 +1,71 @@
+import { draw } from "../../vue/draw.js";
+import { getcanvasWitdh, getcanvasHeight } from "../../vue/canvasInfo.js";
+
+const ennemyWidhtSize = 40;
+const ennemyHeightSize = 40;
+
+export class Ennemy{
+    x;
+    y;
+    ennemySkin;
+    ennemySpeed;
+    ennemyReady;
+
+    constructor(skin, speed, fireRate){
+        setTimeout(()=>{
+            this.spawn();
+        }, 100);
+        this.ennemySpeed = speed;
+        this.ennemySkin = skin;
+        this.image = new Image();
+        this.image.src = skin;
+        this.ennemyReady = false;
+        this.image.addEventListener('load', event =>{
+            //setInterval(this.fire, 1000/fireRate);
+            this.ennemyReady = true;
+        })
+    }
+
+    spawn(){
+        this.x = getcanvasWitdh()-ennemyWidhtSize;
+        this.y = getRandomArbitrary(0+ennemyHeightSize, getcanvasHeight()-ennemyHeightSize);
+    }
+
+    move(){
+        this.x -= this.ennemySpeed;
+    }
+
+    /*
+    fire(){
+        new Projectile();
+    }
+    */
+
+    getX() {
+		return x;
+	}
+
+	getY() {
+		return y;
+	}
+
+	getReady() {
+		return this.ennemyReady;
+	}
+
+    display() {
+		draw(this.image, this.x, this.y, ennemyWidhtSize, ennemyHeightSize);
+	}
+
+    getEnnemyHeight(){
+        return ennemyHeightSize;
+    }
+    
+    getEnnemyWidth(){
+        return ennemyWidhtSize;
+    }
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+}
