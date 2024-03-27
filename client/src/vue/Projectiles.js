6bc40377d17d536b476a5e04c72ff9a4d863e438
alexandre.martel.etu <alexandre.martel.etu@univ-lille.fr>
2024-03-27 09:56:32 +0100

    Projectiles init

---
+++ client/src/vue/Projectiles.js
@@ -0,0 +1,50 @@
+const projectileWidth = 30;
+const projectileHeight = 10;
+
+export class Projectile{
+    x;
+    y;
+    projSkin = '/images/player/${id}.png';
+    projImage;
+    projSpeed;
+    projWay;//if 0 fired by player, if 1 move to left
+    projReady;
+
+    constructor(x, y, speed, way){
+        this.x = x;
+        this.y = y;
+        this.projImage = new Image();
+        this.projImage.src = this.projSkin;
+        this.projSpeed = speed;
+        this.projWay = way;
+        this.projReady = false;
+        this.projImage.addEventListener('load', event =>{
+            this.projReady = true;
+        })
+    }
+
+    getX(){
+        return this.x;
+    }
+    getY(){
+        return this.y;
+    }
+    getReady(){
+        return this.projReady;
+    }
+
+    //if 0 move to right (fired by player)
+    //if 1 move to left (fired by ennemy)
+    move(){
+        if(this.way = 0){
+            this.x+=this.projSpeed;
+        }
+        if(this.way = 1){
+            this.x-=this.projSpeed;
+        }
+    }
+
+    display() {
+		Draw.draw(this.image, this.x, this.y, projectileWidth, projectileHeight);
+	}
+}
