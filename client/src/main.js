97f8e487de9dc464cf9a55b68ee8a5a6f57e8921
Romain <romain.degez.etu@univ-lille.fr>
2024-03-29 16:03:31 +0100

    add documentation to the main.js file

--- client/src/main.js
+++ client/src/main.js
@@ -3,21 +3,30 @@ import Router from './vue/Router.js';
 import { io } from 'socket.io-client';
 import MenuView from './vue/MenuView.js';
 
+/**
+ * Link the client to the server.
+ */
 const socket = io();
+
+/**
+ * Create our application views.
+ */
 const gameView = new GameView(
 	document.querySelector('.viewContent .game'),
 	socket
 );
 const menuView = new MenuView(document.querySelector('.viewContent .menu'));
 
+/**
+ * Initialize the Router.
+ */
 const routes = [
 	{ path: '/', view: menuView },
 	{ path: '/game', view: gameView },
 ];
-
 Router.routes = routes;
-Router.navigate('/');
 
-socket.on('news', data => {
-	console.log(data);
-});
+/**
+ * Loading the first view of the application (the menu).
+ */
+Router.navigate('/');
