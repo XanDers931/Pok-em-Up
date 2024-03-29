7b09c6671a56c2df1179a21093340f76bcb38fb1
Romain <romain.degez.etu@univ-lille.fr>
2024-03-29 16:39:11 +0100

    translate documentation of the Router class

--- client/src/vue/Router.js
+++ client/src/vue/Router.js
@@ -1,28 +1,25 @@
 /**
- * Classe Router qui permet de gérer la navigation dans l'application sans rechargement de page.
- * (Single Page Application)
+ * Router class that allows to manage the navigation in the app between the views.
  */
 export default class Router {
 	/**
-	 * Tableau des routes/vues de l'application.
-	 * @example `Router.routes = [{ path: '/help', view: helpView, title: 'Support' }]`
+	 * *Paths/views table of the application.
+	 * @example `Router.routes = [{ path: '/game', view: gameView}]`
 	 */
 	static routes = [];
 	static currentRoute;
 
 	/**
-	 * Affiche la view correspondant à `path` dans le tableau `routes`
-	 * @param {String} path URL de la page à afficher
+	 * Display the view associated to `path` in the `routes` table.
+	 * @param {String} path URL page to display.
 	 */
 	static navigate(path) {
 		const route = this.routes.find(route => route.path === path);
 		if (route) {
-			// on masque la vue précédente
 			if (this.currentRoute) {
 				this.currentRoute.view.hide();
 			}
 			this.currentRoute = route;
-			// on affiche la nouvelle vue
 			route.view.show(path.substring(route.path.length - 1));
 		}
 	}
