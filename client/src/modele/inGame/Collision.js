960b7438ab944fa5003c44a6d8b685ad1ff1168e
alexandre.martel.etu <alexandre.martel.etu@univ-lille.fr>
2024-04-07 15:46:44 +0200

    update colision function

--- client/src/modele/inGame/Collision.js
+++ client/src/modele/inGame/Collision.js
@@ -1,42 +1,18 @@
 /**
  *
- * @param {the value to check} value
- * @param {the first value of the area} otherValue
- * @param {the size of the area} size
- * @returns true if value is in the range of otherValue to otherValue + size
- * @returns false in the other case
- */
-export function isIn(value, otherValue, size) {
-	return (
-		value > otherValue + size * 0.2 && value < otherValue + size - size * 0.35
-	);
-}
-
-/**
- *
- * @param {List of coordinates off all the Area who deal damage actually} damageAreaList
- * @param {the Player x position} x
- * @param {the Player y position} y
- * @param {the Player width} width
- * @param {the Player height} height
+ * @param {the damager x position} damagerX
+ * @param {the damager y position} damagerY
+ * @param {the damager width} damagerWidth
+ * @param {the damager height} damagerHeight
+ * @param {the hit x position} hitX
+ * @param {the damaged y position} hitY
+ * @param {the damaged width} hitWidht
+ * @param {the damaged height} hitHeight
  * @returns
  */
-export function allColision(damageAreaList, x, y, width, height) {
-	let ret = -1;
-	damageAreaList.forEach(element => {
-		if (
-			isIn(x, element.firstX, element.secondX) ||
-			isIn(x + width, element.firstX, element.secondX) ||
-			isIn(x + width / 2, element.firstX, element.secondX)
-		) {
-			if (
-				isIn(y, element.firstY, element.secondY) ||
-				isIn(y + height, element.firstY, element.secondY) ||
-				isIn(y + height / 2, element.firstY, element.secondY)
-			) {
-				ret = damageAreaList.indexOf(element);
-			}
-		}
-	});
-	return ret;
+export function collider(damagerX, damagerY, damagerWidth, damagerHeight, hitX, hitY, hitWidht, hitHeight){
+	return damagerX < hitX + hitWidht &&
+		   damagerX + damagerWidth > hitX &&
+           damagerY < hitY + hitHeight &&
+           damagerY + damagerHeight > hitY;
 }
